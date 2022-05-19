e4c3e53780e7e7d99af69c1de8746a45499c68f6
TreysiSoto <103224588+TreysiSoto@users.noreply.github.com>
2022-05-19 13:42:31 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,13 @@
+package Clase08;
+
+public class Main {
+
+    public static void main(String args[]) {
+
+        MatrizT transpuesta = new MatrizT();
+        transpuesta.agregarMatriz();
+        transpuesta.mostrarMatriz();
+        transpuesta.calcularMatrizT();
+        transpuesta.mostrarMatrizT();
+    }
+}
